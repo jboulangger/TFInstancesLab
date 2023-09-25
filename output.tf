cb264418a373fea7b48da55cfcdbd0f492587c7b
Joan Boulangger <jboulangger@canvia.com>
2023-09-25 11:41:41 -0500

    Output/Removido

--- output.tf
+++
@@ -1,6 +0,0 @@
-output "ips_publics" {
-  value = {
-    for instance_name, instance in google_compute_instance.instancias :
-    instance_name => instance.network_interface[0].access_config[0].nat_ip
-  }
-}
